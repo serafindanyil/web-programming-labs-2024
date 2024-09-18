92af748000bee50dc1a9082acd5dbb9c486642a8
serafindanyil <danyil.serafin.ir.2023@lpnu.ua>
2024-09-18 13:26:18 +0300

    Add some code to repo

---
+++ js/script.js
@@ -0,0 +1,84 @@
+// Створити клас “Банк” котрий містить поля:
+// - назва
+// - кількість клієнтів
+// - кількість виданих кредитів
+
+class User {
+	constructor(firstName, lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	get data() {
+		return `${this.firstName} ${this.lastName}`;
+	}
+}
+
+class BankOperation extends User {
+	constructor(firstName, lastName) {
+		super(firstName, lastName); // Передаємо ім'я та прізвище батьківському класу
+		this._creditLimit = 1000;
+		this._balance = 0;
+		this._credit = 0;
+	}
+
+	addToDeposit(count) {
+		if (count > 0) {
+			this._balance += count;
+		} else {
+			return console.log("Число має бути позитивне");
+		}
+	}
+
+	// Метод для зняття коштів
+	withdrawFromDeposit(count) {
+		if (count > 0 && this._balance >= count) {
+			this._balance -= count;
+		} else {
+			return console.log("Недостатньо коштів на рахунку");
+		}
+	}
+
+	takeCredit(count) {
+		if (this._credit + count <= this._creditLimit) {
+			if (count > 0) {
+				this._balance += count;
+				this._credit += count;
+			} else {
+				return console.log("Число має бути позитивне");
+			}
+		} else {
+			return console.log("Перевищено кредитний ліміт");
+		}
+	}
+
+	addToCredit(count) {
+		if (count > 0 && this._balance >= count) {
+			this._balance -= count;
+			this._credit -= count;
+		} else {
+			return console.log("Недостатньо коштів на рахунку");
+		}
+	}
+}
+
+const user1 = new BankOperation("Danyil", "Serafin");
+user1.addToDeposit(100);
+console.log(user1);
+
+// class Bank {
+// 	constructor(name, creditSum) {}
+// }
+
+// class Person {
+//   constructor(name, age) {
+//       this.name = name;
+//       this.age = age;
+//   }
+
+//   greet() {
+//       console.log(`Привіт, мене звуть ${this.name}`);
+//   }
+// }
+
+// const john = new Person('John', 30);
+// john.greet();  // Виведе: Привіт, мене звуть John
