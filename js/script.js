8e5ac165e84dbbb61a296a34a87603886b45d103
serafindanyil <danyil.serafin.ir.2023@lpnu.ua>
2024-09-21 17:01:33 +0300

    Add code

--- js/script.js
+++ js/script.js
@@ -3,34 +3,38 @@
 // - кількість клієнтів
 // - кількість виданих кредитів
 
+function selectedUser() {
+	const selectElement = document.getElementById("selectUser");
+	const selectedValue = selectElement.value;
+	return selectedValue;
+}
+
 class User {
-	static userCount = 0;
-
-	constructor(firstName, lastName) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-
-		User.userCount++;
+	constructor(name) {
+		this.name = name;
 	}
 
-	get data() {
-		return `${this.firstName} ${this.lastName}`;
-	}
-
-	get userCount() {
-		return User.userCount;
+	data() {
+		return this.name;
 	}
 }
 
 class BankOperation extends User {
 	static creditCount = 0;
 
-	constructor(firstName, lastName) {
-		super(firstName, lastName); // Передаємо ім'я та прізвище батьківському класу
+	constructor(name) {
+		super(name); // Передаємо ім'я та прізвище батьківському класу
 		this._creditLimit = 1000;
 		this._balance = 0;
 		this._credit = 0;
+
+		// this.updateData(`$ ${this._balance}`, "countMoney");
 	}
+
+	// updateData(value, selector) {
+	// 	const updateValue = document.getElementById(selector);
+	// 	updateValue.textContent = value;
+	// }
 
 	addToDeposit(count) {
 		if (count > 0) {
@@ -38,6 +42,8 @@ class BankOperation extends User {
 		} else {
 			return console.log("Число має бути позитивне");
 		}
+
+		// this.updateData(`$ ${this._balance}`, "countMoney");
 	}
 
 	// Метод для зняття коштів
@@ -62,6 +68,7 @@ class BankOperation extends User {
 		}
 
 		BankOperation.creditCount++;
+		// this.updateData(`$ ${this._balance}`, "countMoney");
 	}
 
 	addToCredit(count) {
@@ -74,24 +81,220 @@ class BankOperation extends User {
 	}
 }
 
-const user1 = new BankOperation("Danyil", "Serafin");
-user1.addToDeposit(100);
-console.log(user1);
+class UserManager {
+	static userObject = [];
+	static userObjectNames = [];
 
-// class Bank {
-// 	constructor(name, creditSum) {}
-// }
+	userCount() {
+		return UserManager.userObject.length;
+	}
 
-// class Person {
-//   constructor(name, age) {
-//       this.name = name;
-//       this.age = age;
-//   }
+	// Метод для додавання нового об'єкта User
+	addUser(name) {
+		const isUserInArray = UserManager.userObjectNames.find(
+			(userName) => userName === name
+		);
 
-//   greet() {
-//       console.log(`Привіт, мене звуть ${this.name}`);
-//   }
-// }
+		if (!isUserInArray) {
+			const user = new BankOperation(name);
 
-// const john = new Person('John', 30);
-// john.greet();  // Виведе: Привіт, мене звуть John
+			UserManager.userObject.push(user);
+			UserManager.userObjectNames.push(name);
+
+			return user; // Повертає створеного користувача
+		} else {
+			console.log("Користувач вже є!");
+		}
+	}
+
+	// Метод для зміни властивості 'name' у User за індексом або іменем
+	getUserByName(name) {
+		const user = UserManager.userObject.find((user) => user.data() === name);
+		return user;
+	}
+
+	listUsers() {
+		return UserManager.userObjectNames;
+	}
+}
+
+const newUser = new UserManager();
+
+// Функція для загального обновлення даних, вона має таку конструкцію через те що,
+// коли у нас display ноне все крім останнього елемента, при створенні юзерів викликається фунція обновлення даних і створюється ексепшииин
+function updateData() {
+	if (newUser.userCount() < 1) {
+		console.log("Юзарів немає, створіть юзера щоб обновити дані");
+	} else {
+		let userIsSelected = selectedUser();
+
+		const changeValue = document.getElementById("countUsers");
+		changeValue.textContent = newUser.userCount();
+
+		const creditLimit = document.getElementById("creditLimitValue");
+		const accessCreditValue = document.getElementById("accessCreditValue");
+		const counterUserCredit = document.getElementById("countUserTakeCredit");
+
+		creditLimit.textContent = `$ ${
+			newUser.getUserByName(userIsSelected)._creditLimit
+		}`;
+		accessCreditValue.textContent = `$ ${
+			newUser.getUserByName(userIsSelected)._creditLimit -
+			newUser.getUserByName(userIsSelected)._credit
+		}`;
+
+		counterUserCredit.textContent = BankOperation.creditCount;
+
+		const updateValue = document.getElementById("countMoney");
+		updateValue.textContent = `$ ${
+			newUser.getUserByName(userIsSelected)._balance
+		}`;
+
+		const countUserPayToCredit = document.getElementById(
+			"countUserPayToCredit"
+		);
+		countUserPayToCredit.textContent = `$ ${
+			newUser.getUserByName(userIsSelected)._credit
+		}`;
+
+		// Оновлюємо ім`я юзера
+		const nameOfUser = document.getElementById("nameOfUser");
+		nameOfUser.textContent = newUser.getUserByName(userIsSelected).name;
+	}
+}
+
+// Створити новий акаунт
+
+// Події на клік створити юзера
+document.getElementById("createUser").addEventListener("click", () => {
+	const name = document.getElementById("userName").value;
+	const changeValue = document.getElementById("countUsers");
+
+	// Селект
+	const addToSelectUser = document.getElementById("selectUser");
+
+	// Створюємо новий елемент option
+	const newOption = document.createElement("option");
+
+	// Додавання юзера
+	if (name.trim() === "") {
+		console.log("Значення не має бути пустим");
+	} else {
+		newUser.addUser(name);
+		// очищення інпуту
+		document.getElementById("userName").value = "";
+		// Додавання нового юзера до селекта
+		newOption.value = name;
+		newOption.text = name;
+		// Оновлення лічильника
+		// changeValue.textContent = newUser.userCount();
+		//Додавання нового юзера до селекта
+		addToSelectUser.add(newOption);
+		// Оновлення лічильника функція
+
+		if (newUser.userCount() >= 1) {
+			// Коли у нас немає юзерів появляється тільки картка з створенням профілів
+			const containerEl = document.querySelector(".container");
+
+			containerEl.classList.remove("container--one-obj");
+			containerEl.classList.add("container--many-obj");
+		}
+
+		updateData();
+	}
+});
+
+// Події на клік взяти креди
+document.getElementById("takeCredit").addEventListener("click", () => {
+	const creditValue = document.getElementById("creditValue").value;
+	const creditLimit = document.getElementById("creditLimitValue");
+	const accessCreditValue = document.getElementById("accessCreditValue");
+	const counterUserCredit = document.getElementById("countUserTakeCredit");
+	let userIsSelected = selectedUser();
+
+	// Додавання юзера
+	if (creditValue.trim() === "") {
+		console.log("Значення не має бути пустим");
+	} else {
+		newUser.getUserByName(userIsSelected).takeCredit(Number(creditValue));
+		// очищення інпуту
+		document.getElementById("creditValue").value = "";
+		// Оновлення лічильника
+		// creditLimit.textContent =
+		// 	newUser.getUserByName(userIsSelected)._creditLimit;
+		// accessCreditValue.textContent =
+		// 	newUser.getUserByName(userIsSelected)._creditLimit -
+		// 	newUser.getUserByName(userIsSelected)._credit;
+		// counterUserCredit.textContent = BankOperation.creditCount;
+
+		// Оновлення лічильника функція
+		updateData();
+	}
+});
+
+// Погасити кредит
+document.getElementById("payToCredit").addEventListener("click", () => {
+	const payToCreditValue = document.getElementById("payToCreditValue").value;
+
+	let userIsSelected = selectedUser();
+
+	// Додавання юзера
+	if (payToCreditValue.trim() === "") {
+		console.log("Значення не має бути пустим");
+	} else {
+		newUser.getUserByName(userIsSelected).addToCredit(Number(payToCreditValue));
+		// очищення інпуту
+		document.getElementById("payToCreditValue").value = "";
+
+		// Оновлення лічильника функція
+		updateData();
+	}
+});
+
+const selectElement = document.getElementById("selectUser");
+
+// Обновлює дані коли переключається між юзерами
+selectElement.addEventListener("change", function () {
+	updateData();
+});
+
+// Відслідковування кнопок коли нажимається ентер
+
+// створити профіль
+const userNameEl = document.getElementById("userName");
+const createUserEl = document.getElementById("createUser");
+
+// Відстежуємо натискання клавіш на input
+userNameEl.addEventListener("keydown", function (event) {
+	// Перевіряємо, чи натиснута клавіша Enter (код 13 або 'Enter')
+	if (event.key === "Enter") {
+		// Спрацьовує кнопка
+		createUserEl.click();
+	}
+});
+
+// Взяти кредит
+const creditValueEl = document.getElementById("creditValue");
+const takeCreditEl = document.getElementById("takeCredit");
+
+// Відстежуємо натискання клавіш на input
+creditValueEl.addEventListener("keydown", function (event) {
+	// Перевіряємо, чи натиснута клавіша Enter (код 13 або 'Enter')
+	if (event.key === "Enter") {
+		// Спрацьовує кнопка
+		takeCreditEl.click();
+	}
+});
+
+// Погасити кредит
+const payToCreditValueEl = document.getElementById("payToCreditValue");
+const payToCreditEl = document.getElementById("payToCredit");
+
+// Відстежуємо натискання клавіш на input
+payToCreditValueEl.addEventListener("keydown", function (event) {
+	// Перевіряємо, чи натиснута клавіша Enter (код 13 або 'Enter')
+	if (event.key === "Enter") {
+		// Спрацьовує кнопка
+		payToCreditEl.click();
+	}
+});
